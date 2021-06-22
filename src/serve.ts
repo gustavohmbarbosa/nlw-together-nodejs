0b2c9a2122dc6b67cc5ecd6f69a452d56fe1c34e
Gustavo Melo <gustavobarbosa-18@hotmail.com>
2021-06-21 23:24:51 -0300

    aula 2

--- src/serve.ts
+++ src/serve.ts
@@ -1,11 +1,17 @@
-import express from 'express';
+import "reflect-metadata";
+import express from "express";
+import { router } from "./routes";
+
+import "./database";
 
 // @types/express
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send("Hello"));
+app.use(express.json());
+
+app.use(router);
 
 app.listen(port, () => {
-    console.log(`server is running at http://localhost:${port}`);
-})
+  console.log(`server is running at http://localhost:${port}`);
+});
